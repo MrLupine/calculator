83ecac169ae1a4409cb636732f50309cd1992227
Josse Pickard <jossepickard@gmail.com>
2020-04-01 18:13:34 +0100

    Finished tweaking the appearance and started adding javascript

--- js/scripts.js
+++ js/scripts.js
@@ -21,13 +21,102 @@ const equals = document.getElementsByClassName("calculator--equals")
 .addEventListener("click", )
 */
 
-let sum = 0
+const calculator = {
+	readout: "0", 
+	operand: "",
+	sum: [],
+}
 
-document.addEventListener("click", (e) => {
-	console.log(e.target);
+const calculatorButtonMap = {
+	zero: 0,
+	one: 1,
+	two: 2,
+	three: 3,
+	four: 4,
+	five: 5,
+	six: 6,
+	seven: 7,
+	eight: 8,
+	nine: 9,
+	plus: "+",
+	minus: "-",
+	multiply: "*",
+	divide: "/",
+	percentage: "%",
+	decimalPoint: ".",
+}
+
+const calculatorButtons = document.querySelectorAll(".calculator__button")
+calculatorButtons.forEach(button => {
+	button.addEventListener("click", (e) => readoutFunction(e))
 })
 
-document.getElementsByClassName("calculator__solar-panel").addEventListener('mouseover', () => {
-	const readout = document.getElementsByClassName("calculator__readout")
-	
+function readoutFunction(e) {
+	const readout = document.getElementById("readout");
+	const datatype = e.target.dataset.type;
+	if (!isNaN(calculatorButtonMap[datatype]) && maxReadout()) {
+		if (calculator.readout === "0") {
+			calculator.readout = `${calculatorButtonMap[datatype]}`;
+		} else {
+			calculator.readout += `${calculatorButtonMap[datatype]}`;
+		}
+		readout.innerHTML = calculator.readout;
+		spanTagCreation();
+	} else {
+		calculator.operand = calculatorButtonMap[button];
+		const readoutOperand = [calculator.readout, calculator.operand];
+		calculator.sum.push(...readoutOperand);
+		calculator.readout = "0";
+	}
+}
+
+const maxReadout = () => (calculator.readout.includes(".") && calculator.readout.length <= 9) ? true : (calculator.readout.length <= 8) ? true : false;
+
+function spanTagCreation() {
+	readout.innerHTML = readout.innerHTML.replace(/\./, `<span class="calculator__readout--decimal-point-spacing">.</span>`);
+	readout.innerHTML = readout.innerHTML.replace(/1/g, `<span class="calculator__readout--one-spacing">1</span>`);
+}
+
+document.addEventListener("keydown", (e) => {
 })
+
+document.getElementById("solar--panel").addEventListener('mouseover', () => {
+	const readout = document.getElementById("readout")
+	const readoutDP = document.getElementById("readout-decimal-point");
+	readout.style.opacity = "0";
+	readoutDP.style.opacity = "0";
+})
+
+document.getElementById("solar--panel").addEventListener('mouseout', () => {
+	const readout = document.getElementById("readout");
+	const readoutDP = document.getElementById("readout-decimal-point");
+	readout.style.opacity = "1.0";
+	readoutDP.style.opacity = "1.0";
+})
+
+const operator = (operand, x, y) => {
+	const add = (x, y) => x + y;
+	const minus = (x, y) => x - y;
+	const multiply = (x, y) => x * y
+	const divide = (x, y) => (y === 0) ? "undefined" : x / y;
+
+	switch (operand) {
+		case "+": 
+			return add(x, y)
+			break;
+		case "-": 
+			return minus(x, y)
+			break;
+		case "*": 
+			return multiply(x, y)
+			break;
+		case "/": 
+			return divide(x, y)
+			break;
+		default:
+			"error"
+	}
+}
+
+
+
