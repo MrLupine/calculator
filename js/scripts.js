abe16b77c259a580a36d7bafcb2d6cb931d2f367
Josse Pickard <jossepickard@gmail.com>
2020-04-07 14:44:02 +0100

    wired up keyboard, edited functions so they run more efficiently.

--- js/scripts.js
+++ js/scripts.js
@@ -1,84 +1,148 @@
-/*const readout = document.getElementsByClassName("calculator__readout")
-const ac = document.getElementsByClassName("calculator--ac")
-const plusMinus = document.getElementsByClassName("calculator--plus-minus")
-const percentage = document.getElementsByClassName("calculator--percentage")
-const divide = document.getElementsByClassName("calculator--divide")
-const numSeven = document.getElementsByClassName("calculator--seven")
-const numEight = document.getElementsByClassName("calculator--eight")
-const numNine = document.getElementsByClassName("calculator--nine")
-const multiply = document.getElementsByClassName("calculator--multiply")
-const numFour = document.getElementsByClassName("calculator--four")
-const numFive = document.getElementsByClassName("calculator--five")
-const numSix = document.getElementsByClassName("calculator--six")
-const minus = document.getElementsByClassName("calculator--minus")
-const numOne = document.getElementsByClassName("calculator--one")
-const numTwo = document.getElementsByClassName("calculator--two")
-const numThree = document.getElementsByClassName("calculator--three")
-const numZero = document.getElementsByClassName("calculator--plus")
-const decimalPoint = document.getElementsByClassName("calculator--decimal-point")
-const equals = document.getElementsByClassName("calculator--equals")
-
-.addEventListener("click", )
-*/
-
-const calculator = {
-	readout: "0", 
-	operand: "",
-	sum: [],
-}
-
-const calculatorButtonMap = {
-	zero: 0,
-	one: 1,
-	two: 2,
-	three: 3,
-	four: 4,
-	five: 5,
-	six: 6,
-	seven: 7,
-	eight: 8,
-	nine: 9,
-	plus: "+",
-	minus: "-",
-	multiply: "*",
-	divide: "/",
-	percentage: "%",
-	decimalPoint: ".",
-}
-
 const calculatorButtons = document.querySelectorAll(".calculator__button")
 calculatorButtons.forEach(button => {
-	button.addEventListener("click", (e) => readoutFunction(e))
+	button.addEventListener("mouseup", (e) => calcFunction(e.target.dataset.type, e))
 })
-
-function readoutFunction(e) {
-	const readout = document.getElementById("readout");
-	const datatype = e.target.dataset.type;
-	if (!isNaN(calculatorButtonMap[datatype]) && maxReadout()) {
-		if (calculator.readout === "0") {
-			calculator.readout = `${calculatorButtonMap[datatype]}`;
-		} else {
-			calculator.readout += `${calculatorButtonMap[datatype]}`;
-		}
-		readout.innerHTML = calculator.readout;
-		spanTagCreation();
-	} else {
-		calculator.operand = calculatorButtonMap[button];
-		const readoutOperand = [calculator.readout, calculator.operand];
-		calculator.sum.push(...readoutOperand);
-		calculator.readout = "0";
-	}
-}
-
-const maxReadout = () => (calculator.readout.includes(".") && calculator.readout.length <= 9) ? true : (calculator.readout.length <= 8) ? true : false;
-
-function spanTagCreation() {
-	readout.innerHTML = readout.innerHTML.replace(/\./, `<span class="calculator__readout--decimal-point-spacing">.</span>`);
-	readout.innerHTML = readout.innerHTML.replace(/1/g, `<span class="calculator__readout--one-spacing">1</span>`);
-}
 
 document.addEventListener("keydown", (e) => {
+	calcFunction(e.key, e);
 })
+
+const calculator = {
+	readout: "", 
+	operand: "",
+	inputs: [],
+}
+
+function calcFunction(input) {
+	const decimalPoint = document.getElementById("readout-decimal-point")
+	const readout = document.getElementById("readout");
+	const operands = ["+", "-", "*", "/"]
+
+	const decimalPointCheck = () => (readout.innerHTML.includes("."))
+	const decimalPointReplace = () => (!decimalPointCheck()) ? decimalPoint.innerHTML = `<span class="calculator__readout--decimal-point-spacing">.</span>` : decimalPoint.innerHTML = ""
+	const inputNum = () => calculator.inputs.push(Number(calculator.readout))	
+	const maxReadout = () => (calculator.readout.includes(".") && calculator.readout.length <= 9) ? true : (calculator.readout.length <= 8);	
+	const maxReadoutHTML = () => (readout.innerHTML.includes(".") && readout.innerHTML.length <= 10) ? true : (readout.innerHTML.length <= 9);
+	const operandCheck = () => (operands.includes(`${input}`));
+	const operator = (operand, x, y) => {
+		const add = (x, y) => x + y;
+		const minus = (x, y) => x - y;
+		const multiply = (x, y) => x * y
+		const divide = (x, y) => (y === 0) ? "undefined" : x / y;
+		const totalLimiter = (x) => {
+			const value = x.toString()
+			let indexOf = (indexItem) => value.indexOf(`${indexItem}`)
+			if (value.includes(".") && indexOf(".") < 9 && value.length > 10) {
+				if (indexOf("0.") === 0 && !Number(value.slice(2, 10))) {
+					const exponent = `${Math.abs(Math.log10(value))}`;
+					return x.toExponential(6 - exponent.length);
+				} else {
+					return x.toFixed(9 - indexOf("."));
+				}
+			} else if (value.length > 9) {
+				return x.toExponential(4);
+			} else {
+				return x;
+			}
+		}
+
+		switch (operand) {
+			case "+": 
+				return totalLimiter(add(x, y))
+				break;
+			case "-": 
+				return totalLimiter(minus(x, y))
+				break;
+			case "*": 
+				return totalLimiter(multiply(x, y))
+				break;
+			case "/": 
+				return totalLimiter(divide(x, y))
+				break;
+			default:
+				"error"
+		}
+	}
+	const CalcWordsFlip = () => {
+		if (calculator.readout === "5318008" || calculator.readout === "0.1134") {
+			const calculator = document.getElementById("calculator");
+			const resetFlip = () => calculator.style.transform = "";
+			calculator.style.transform = "rotate(180deg)";
+			setTimeout(resetFlip, 6000);
+		}
+	}
+	const spanTagCreation = () => {
+		readout.innerHTML = readout.innerHTML.replace(/\./, `<span class="calculator__readout--decimal-point-spacing">.</span>`);
+		readout.innerHTML = readout.innerHTML.replace(/1/g, `<span class="calculator__readout--one-spacing">1</span>`);
+	}
+	const updateDomReadout = (x) => {
+		calculator.readout = `${x}`
+		readout.innerHTML = calculator.readout;
+	}
+
+	if (!isNaN(input) && maxReadout()) {
+		if (calculator.readout === "0" && input === 0) {
+			return
+		}
+		updateDomReadout(calculator.readout + `${input}`);
+		CalcWordsFlip();
+	} else if (input === "." && !decimalPointCheck()) {
+		if (calculator.readout === "" && input === ".") {
+			updateDomReadout(calculator.readout + "0.");
+		} else {
+			updateDomReadout(calculator.readout + ".");
+		}
+	} else if (operandCheck(input)) {
+		if (!calculator.operand && calculator.readout) {
+			inputNum();
+			calculator.operand = input;
+			calculator.readout = "";
+		} else if (calculator.operand && !calculator.readout) {
+			calculator.operand = input;
+		} else if (!calculator.inputs.length && !calculator.readout) {
+			calculator.inputs.push(Number(readout.innerHTML));
+			calculator.operand = input;
+			calculator.readout = "";
+		} else {
+			inputNum()
+			updateDomReadout(operator(calculator.operand, calculator.inputs[0], calculator.inputs[1]))
+			calculator.operand = input;
+			calculator.inputs = [(Number(calculator.readout))];
+			calculator.readout = "";
+		}
+	} else if (input === "%") {
+		if (calculator.inputs[0]) {
+			updateDomReadout(calculator.inputs[0]/100 * calculator.readout);
+		} else {
+			updateDomReadout(calculator.readout / 100);
+		}
+	} else if (input === "=" || input === "Enter") {
+		if (!calculator.inputs[1] && !calculator.operand) {
+			return;
+		} else {
+			inputNum();
+			updateDomReadout(operator(calculator.operand, calculator.inputs[0], calculator.inputs[1]))
+			calculator.readout = "";
+			calculator.operand = "";
+			calculator.inputs = [];
+		}
+	} else if (input === "ac" || input === "Backspace") {
+		if (calculator.readout && input === "Backspace") {
+			updateDomReadout(calculator.readout.slice(0, -1));
+		} else if (calculator.readout && input === "ac") {
+			updateDomReadout("")
+		} else {
+			calculator.readout = ""; 
+			calculator.operand = "";
+			calculator.inputs = [];
+			readout.innerHTML = "0";
+		}
+	}
+	if (maxReadoutHTML()) {
+		spanTagCreation();
+	}
+	decimalPointReplace();
+}
 
 document.getElementById("solar--panel").addEventListener('mouseover', () => {
 	const readout = document.getElementById("readout")
@@ -93,30 +157,3 @@ document.getElementById("solar--panel").addEventListener('mouseout', () => {
 	readout.style.opacity = "1.0";
 	readoutDP.style.opacity = "1.0";
 })
-
-const operator = (operand, x, y) => {
-	const add = (x, y) => x + y;
-	const minus = (x, y) => x - y;
-	const multiply = (x, y) => x * y
-	const divide = (x, y) => (y === 0) ? "undefined" : x / y;
-
-	switch (operand) {
-		case "+": 
-			return add(x, y)
-			break;
-		case "-": 
-			return minus(x, y)
-			break;
-		case "*": 
-			return multiply(x, y)
-			break;
-		case "/": 
-			return divide(x, y)
-			break;
-		default:
-			"error"
-	}
-}
-
-
-
